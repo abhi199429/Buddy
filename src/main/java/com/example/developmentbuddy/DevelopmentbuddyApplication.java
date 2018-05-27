82d9ae5c373a4448d401848dc8d4735b5240285d
Abhishek Bhatt <abhishekbhattabc@gmail.com>
2018-05-27 13:26:41 -0700

    Initial commit for hello world demo

---
+++ src/main/java/com/example/developmentbuddy/DevelopmentbuddyApplication.java
@@ -0,0 +1,12 @@
+package com.example.developmentbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentbuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopmentbuddyApplication.class, args);
+    }
+}
