67aeda2a35bcafeca54d5a9d6ee6a8b94c389690
Abhishek Bhatt <abhishekbhattabc@gmail.com>
2018-05-27 03:36:52 -0700

    Commit for Spring STS

---
+++ src/main/java/com/Abhishek/DevelopmentDemo/DevelopmentDemoApplication.java
@@ -0,0 +1,12 @@
+package com.Abhishek.DevelopmentDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentDemoApplication.class, args);
+	}
+}
