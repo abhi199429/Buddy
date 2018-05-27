67341eb6b33a8e2ce4b807492a52233289831b8a
Abhishek Bhatt <abhishekbhattabc@gmail.com>
2018-05-27 13:34:12 -0700

    Commit after first test of modules to be implemented

---
+++ src/main/java/com/Abhishek/Controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.Abhishek.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String sayHello(){
+        return "index";
+    }
+
+}
